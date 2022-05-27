9c526de9ef7bee0b3b9ddd4898c21ac1036b2c85
Vishvaroop <vishvarooppatra@gmail.com>
2022-05-27 21:04:56 +0200

    In Progress - Code First EF Core

---
+++ LundSchoolLocations.Models/Locations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LundSchoolLocations.Models
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Location> Locations { get; set; }        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Location>().ToTable("Location");            
+        }
+    }
+
+    public class Location
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string PostCode { get; set; }
+    }
+}
---
+++ PubllicData/SchoolLocations.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[SchoolLocations]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [street] NCHAR(10) NULL, 
+    [postcode] NCHAR(10) NULL, 
+    [name] NCHAR(10) NULL, 
+    [locality] NCHAR(10) NULL, 
+    [source] NCHAR(10) NULL, 
+    [type] NCHAR(10) NULL, 
+    [operation] NCHAR(10) NULL, 
+    [latitiude] NCHAR(10) NULL, 
+    [longitude] NCHAR(10) NULL, 
+    [URL] NCHAR(10) NULL
+)
